52a12a66ba627b83c5d2654d9aac58b835a3aa0a
surya432 <hadisurya295@gmail.com>
2020-03-12 17:21:32 +0700

    add toolbar

--- src/Pages/screens/HomeScreen.js
+++ src/Pages/screens/HomeScreen.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
-import { Text, View ,SafeAreaView,StyleSheet} from 'react-native'
-
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native'
+import TopBar from './../component/TopBar'
 export class HomeScreen extends Component {
     render() {
         return (
             <SafeAreaView>
-            <View style={styles.container}>
-                <View style={styles.content} >
-                    <Text> SettingsScreen </Text>
+                <View style={styles.container}>
+                    <View style={styles.statusBar} />
+                    <TopBar/>
                 </View>
-            </View>
-        </SafeAreaView>
+            </SafeAreaView>
         )
     }
 }
