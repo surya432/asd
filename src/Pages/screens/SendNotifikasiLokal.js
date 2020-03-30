47bfa8556c58a87eb64808b0d7cd479e5f2a2736
surya432 <hadisurya295@gmail.com>
2020-03-30 14:04:47 +0700

    add firebase

--- src/Pages/screens/SendNotifikasiLokal.js
+++ src/Pages/screens/SendNotifikasiLokal.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View, SafeAreaView } from 'react-native'
 import GlobalStyles from '../Components/GlobalStyles'
-import messaging from '@react-native-firebase/messaging';
-import { firebase } from '@react-native-firebase/messaging';
+// import messaging, { firebase } from '@react-native-firebase/messaging';
+import firebase from '@react-native-firebase/app';
+import '@react-native-firebase/messaging';
 export default class SendNotifikasiLokal extends Component {
+    constructor() {
+        super()
+    }
+
     async componentDidMount() {
         this._registerFcm();
     }
@@ -18,7 +23,7 @@ export default class SendNotifikasiLokal extends Component {
             });
     }
 
-    _returnFcmToken = () => new Promise(async (resolve) => {
+    ReturnFcmToken = () => new Promise(async (resolve) => {
         try {
             const fcmToken = await firebase.messaging().getToken();
             // you can put here some logic like storing fcmToken in sharedpreferences
@@ -41,12 +46,15 @@ export default class SendNotifikasiLokal extends Component {
     _checkPermissionAndReturnFcmToken = () => new Promise(async (resolve, reject) => {
         const enabled = await firebase.messaging().hasPermission();
         if (enabled) {
-            const fcmToken = await ReturnFcmToken();
+            const fcmToken = await this.ReturnFcmToken();
+
+            console.log(fcmToken);
             resolve(fcmToken);
         } else {
             try {
                 await RequestPermission();
-                const fcmToken = await ReturnFcmToken();
+                const fcmToken = await this.ReturnFcmToken();
+                console.log(fcmToken);
                 resolve(fcmToken);
             } catch (e) {
                 reject(e);
